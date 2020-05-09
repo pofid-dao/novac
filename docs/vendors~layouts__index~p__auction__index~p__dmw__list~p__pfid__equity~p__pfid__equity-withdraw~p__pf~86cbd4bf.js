2a187b3467ee678c63c8f9d4b290e287803a194d
SaintGo <saintgo@protonmail.com>
2020-05-09 15:53:04 +0800

    update docs

--- docs/vendors~layouts__index~p__auction__index~p__dmw__list~p__pfid__equity~p__pfid__equity-withdraw~p__pf~86cbd4bf.js
+++ docs/vendors~layouts__index~p__auction__index~p__dmw__list~p__pfid__equity~p__pfid__equity-withdraw~p__pf~86cbd4bf.js
@@ -10452,7 +10452,7 @@
             npm_config_only: '',
             npm_config_commit_hooks: 'true',
             npm_config_browser: '',
-            npm_package_gitHead: '25b5375cc3945220a1babd8d3d3d14838f048311',
+            npm_package_gitHead: '60971d833981525d90fed6d98fb7bbde991a2da9',
             npm_package_devDependencies_i18next_http_backend: '^1.0.8',
             npm_config_also: '',
             npm_config_sign_git_commit: '',
